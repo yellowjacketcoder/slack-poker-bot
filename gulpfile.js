662e7cb34de0c665429b352ecdf40ff80d0137eb
verveguy <bpjadam@gmail.com>
2020-04-06 23:37:02 -0400

    Allow for testing images alone

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,8 @@ gulp.task('default', function () {
   return gulp.src('tests/*.js', {read: false})
     .pipe(mocha({reporter: 'spec'}));
 });
+
+gulp.task('jtest', function () {
+  return gulp.src('tests/image-helpers-spec.js', {read: false})
+    .pipe(mocha({reporter: 'spec'}));
+});
